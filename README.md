14b7ffb3ee42d132a2c92082c861d2b794bb8696
Rodrigo Berlochi <rodrigo.berlochi@gmail.com>
2019-03-24 20:21:44 -0300

    Clean code. Added comments. Readme progress and roadmap file

--- README.md
+++ README.md
@@ -1,23 +1,46 @@
-# redux-magic-tree
+# redux-context-container
 
-createMagicTree(mapStateToProps, mapDispatchToProps)('domainname');
+## Summary
 
-/\*\*
+With the implementation of Context in React, discussions arised regarding Redux being replaced by React Context. There are great articles about it, that I include in the Further Readings section. There are also poor discussions about this, that sometimes are built on top of a lack of knowledge about React and Redux. 
 
-- Inputs
-- - context name?
-- - defaultVals for context?
-- - presentational components that init the consumer tree
-- - a mapState callback
-- - a mapDispatch callback
--
--
-- Outputs
-- - a container component, a class?, that returns children wrapped by Provider, and which is already connected to the Redux Store
-- - a HOC to wrapp inner tree starter in a consumer
-- - separate dispatch props
--
-- no puede devolver el container hasta que no tenga los dos MAPS, y el defaultVal para context
--
+Context is long-awaited feature of React, and in some use cases it can work pretty well as the State Management approach. Redux, on the other hand, is the defacto State Manager for React applications. And even when you might not need Redux, for thousands of uses cases and big companies around the world, it is still the right answer.
 
-\*/
+React Context as State Manager provides simplicity and a consice code. It lacks however of mechanisms and standards that Redux provides and that stick our application to the Flux architecture (which is part of the React world too). Actions to update the State, Store design for an unidirectional data flow, time travel debugging, redux dev tools, thunks and sagas for async flows, etc. If your are wondering about how to get all that when replacing Redux with Context, you might need Redux. It is a good way to avoid re-inventing the wheel. The study of Redux documentation can tell us the patterns to write more concise and correct Redux applications.
+
+If it is true that some use cases could work better with just Context and other with Redux, it's also true that some people find a third scenario where Redux and Context could play together. This small library tries to express that architectural concept in a tangible way. And using it you should be able to experiment with that pattern without writing a repetitve code.
+
+This library, hence, is and experiment of those concepts expressed at theoretical level. 
+
+## The Pattern
+
+Using Redux in our React application results in a Flux architecture, plus a useful central store, a standard and predicatable pattern to trigger changes in our application, techniques to test our code and tools to debug it. 
+
+When we add React Context to the previous schema, we get an extra advantage: we can create Containers with the ability to pass properties to their children trees without the need of doing it explicetely. That's an interesting experiment. Until now, the way to avoid props drilling was Redux connect() on each component that needs access to some State attribute. And then to pass required properties down in the tree adding them to each level-component. That's the correct way. 
+
+Here we have a second approach. We still use Redux connect() but only for Container components. Each Container implements internally a Context, where the slice of the State defined inside the connect() method, lives accesible for all of the children of that Container, no matter how deep nested they are. Children have access thanks to the Consumer of that Container. 
+
+The architectural proposal was defined in several layers:
+
+- A Global State layer: the usual Redux Store
+
+- A Containers layer: usual Containers from React-Redux applications. But now each one create a React Context with a slice of the global State. And export a Consumer for the next layer. This layer connects() with Redux Store. 
+
+- A root-tree layer: one root component per UI tree. This component's level make use of the Consumer
+
+- Presentational components layer: here resides all the also known as dumb components, focused on rendering view fragments and decoupled from the State implementation 
+
+## Usage
+```javascript
+// the lib has a single default export. That is function that creates this
+// special kind of containers
+import createReduxContextContainer from 'redux-context-container';
+
+createReduxContextContainer(mapStateToProps, mapDispatchToProps)('domainname');
+
+```
+
+
+## Performance and other Considerations
+
+## Further Readings
--- src/redux-context-container.js
+++ src/redux-context-container.js
@@ -1,45 +1,60 @@
-import React, { createContext, Component } from "react";
-import { connect } from "react-redux";
-import { capitalize } from "lodash.capitalize";
+import React, { createContext } from 'react';
+import { connect } from 'react-redux';
+import { capitalize } from 'lodash.capitalize';
 
+/**
+ * We mimic the connect() signature, since internally we will take care of connect()
+ * for this container-component.
+ * The returned function must be invoked with the name of the current Domain, as a recommendation.
+ * If not, generated members won't include any reference in their names to the Domain.
+ * A Domain is an abstraction of a concern of our application, that owns a slice of the State
+ * and is rendered in an specific UI section.
+ * @param {Object|null} mapStateToProps https://react-redux.js.org/using-react-redux/connect-mapstate
+ * @param {Object|null} mapDispatchToProps https://react-redux.js.org/using-react-redux/connect-mapdispatch
+ * @returns {Function}
+ */
 export default (mapStateToProps = null, mapDispatchToProps = null) => (
-  domain = ""
+  domain = '',
 ) => {
-  if (mapStateToProps !== null && typeof mapStateToProps !== "function") {
+  // Validations
+  if (mapStateToProps !== null && typeof mapStateToProps !== 'function') {
     throw new Error(
-      "Argument: if mapStateToProps is defined it must be a function"
+      'Argument: if mapStateToProps is defined it must be a function',
     );
   }
 
   if (
-    mapDispatchToProps !== null &&
-    typeof mapDispatchToProps !== "function" &&
-    typeof mapDispatchToProps !== "object"
+    mapDispatchToProps !== null
+    && typeof mapDispatchToProps !== 'function'
+    && typeof mapDispatchToProps !== 'object'
   ) {
     throw new Error(
-      "Argument: if mapDispatchToProps is defined it must be a function or an object"
+      'Argument: if mapDispatchToProps is defined it must be a function or an object',
     );
   }
 
-  if (typeof domain !== "string") {
-    throw new Error("Argument: domain must a String");
+  if (typeof domain !== 'string') {
+    throw new Error('Argument: domain must a String');
   }
 
+  // React Context creation for this Container
   const Context = createContext();
   const { Consumer, Provider } = Context;
 
   /**
    * @description HOC wraps easily a Presentational component that
    * is usually direct child of the corresponding container and
-   * is also parent of a presentational tree.
+   * is also parent of a presentational tree (Root tree layer).
    * Every child of the returned component will get access to
    * all of the props for this Domain/Container, without need of
    * passing them explicitly on each level.
    * @param {React.Component} Component
    * @returns {React.Component}
    */
-  const withContext = Component => {
+  function withContext(Component) {
     return function WrapperComponent(props) {
+      // the new component get all the props passed manually
+      // but also we spread context attributes as properties
       return (
         <Consumer>
           {context => {
@@ -49,8 +64,19 @@ export default (mapStateToProps = null, mapDispatchToProps = null) => (
       );
     };
   };
-
-  class innerContainer extends Component {
+  
+  /**
+   * Basic scaffolding for a Context-Container
+   * It is a React component that must be able to hangle a Children tree
+   * which is returned wrapped in the Provider that links to the Context specific 
+   * to this Container. 
+   * Each Container conceptually represents a Domain of our app (state slice/UI fragment)
+   * The Context-Container must get all of the properties received (except the children which is
+   * a React tree) and set them as Values for this Context. Those props are taken directly 
+   * from the Redux Store in a next step. 
+   * @see https://redux.js.org/basics/usage-with-react#presentational-and-container-components
+   */
+  class innerContainer extends React.Component {
     render() {
       const { children, ...restProps } = this.props;
 
@@ -58,17 +84,25 @@ export default (mapStateToProps = null, mapDispatchToProps = null) => (
     }
   }
 
+  // just for semantic when we have several Containers, 
+  // we use the Domain name to identify exported memebers 
+  // fo this Container
   const Domain = capitalize(domain);
   const hocName = `with${Domain}Context`;
   const containerName = `${Domain}Container`;
 
+  // Now we tale care of binding the created Container 
+  // to the Redux Store, as usual in any Redux app
+  // Users shouldn't need to do it by themselves
+  // just to express required props and dispatch methods
   const Container = connect(
     mapStateToProps,
-    mapDispatchToProps
+    mapDispatchToProps,
   )(innerContainer);
 
+  // Public API to use on our app
   return {
     [hocName]: withContext,
-    [containerName]: Container
+    [containerName]: Container,
   };
 };
